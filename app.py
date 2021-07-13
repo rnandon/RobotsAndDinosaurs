44e3c73d01371a6e09d8328d19c7ddca6fd6c70d
Ryan <ryan@ryannd.com>
2021-07-13 11:38:36 -0500

    Scaffolded class structure

---
+++ app.py
@@ -0,0 +1,60 @@
+class Battlefield:
+    def __init__(self):
+        pass
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle(self):
+        pass
+
+    def dino_turn(self, dinosaur):
+        pass
+
+    def robo_turn(self, robot):
+        pass
+
+    def show_dino_opponent_options(self):
+        pass
+
+    def show_robo_opponent_options(self):
+        pass
+
+    def display_winners(self):
+        pass
+
+class Fleet:
+    def __init__(self):
+        pass
+
+    def create_fleet(self):
+        pass
+
+class Herd:
+    def __init__(self):
+        pass
+
+    def create_herd(self):
+        pass
+
+class Robot:
+    def __init__(self, name):
+        pass
+
+    def attack(self, dinosaur):
+        pass
+
+class Weapon:
+    def __init__(self, name, attack_power):
+        pass
+
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        pass
+
+    def attack(self, robot):
+        pass
+

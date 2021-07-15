263f54b413b68c26a0fa6baf942a2e0b12ecc211
Ryan <ryan@ryannd.com>
2021-07-15 11:43:42 -0500

    Modified Combatant attack and defend methods to return damage, added get_attack_power and set_attack_power

--- Combatant.py
+++ Combatant.py
@@ -1,16 +1,30 @@
 # Combatant class: parent class for all combatants, implements minimum requirements for each class
 class Combatant:
-    def __init__(self, name, attack_power):
+    def __init__(self, name, attack_power=0):
         self.name = name
         self.health = 100
         self.resource = 100
         self.attack_power = attack_power
 
-    # Deal damage to the selected dinosaur if the robot has enough power
+    ###        METHODS
+    ### =========================================
     def attack(self, opponent):
         if self.resource > 10:
-            opponent.defend(self)
+            damage_dealt = opponent.defend(self)
             self.resource -= 10
+            return damage_dealt
+        else:
+            return None
 
     def defend(self, opponent):
-        self.health -= opponent.get_attack_power()
+        damage = opponent.get_attack_power()
+        self.health -= damage
+        return damage
+
+    ###        GETTERS/SETTERS
+    ### =========================================
+    def get_attack_power(self):
+        return self.attack_power
+
+    def set_attack_power(self, attack_power):
+        self.attack_power = attack_power

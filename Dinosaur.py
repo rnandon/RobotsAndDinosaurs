2be70fe3f60333810dcf5e8ddcf2e443a8b5e8b0
Ryan <ryan@ryannd.com>
2021-07-15 12:02:05 -0500

    Refactored Dinosaur and Robot classes to inherit from Combatant

--- Dinosaur.py
+++ Dinosaur.py
@@ -1,46 +1,19 @@
 ## Imports
 ######################################
-import time
+from Combatant import Combatant
 from random import randint
 
 
 # Dinosaur. Basics are the same as the robot, but it has its own attack power, energy instead of power, and has multiple attacks to choose from 
-class Dinosaur:
-    def __init__(self, name, attack_power):
-        self.name = name
-        self.attack_power = attack_power
-        self.health = 100
-        self.energy = 100
+class Dinosaur(Combatant):
+    def __init__(self, name, attack_power, energy=100):
         self.attack_names = ("Swipe", "Slash", "Stomp")
+        super(Dinosaur, self).__init__(name, attack_power, energy)
 
-    # Deal damage to selected robot if dinosaur has enough energy 
-    def attack(self, robot):
-        if self.energy >= 10:
-            attack = self.attack_names[randint(0, len(self.attack_names) - 1)]
-            robot.health -= self.attack_power
-            if robot.health < 0:
-                robot.health = 0
-            self.energy -= 10
-            print(f"     {self.name.rstrip()} attacks {robot.name} with {attack} for {self.attack_power} damage!")
-            time.sleep(1)
-        else:
-            print(f"     {self.name.rstrip()} doesn't have the energy to attack!")
-            time.sleep(1)
-
-    # Special formatting for health value
-    def get_health(self):
-        if self.health > 99:
-            return f'{self.health}'
-        if self.health > 9:
-            return f'{self.health} '
-        if self.health <= 9:
-            return f'{self.health}  '
-
-    # Special formatting for energy value
-    def get_energy(self):
-        if self.energy > 99:
-            return f'{self.energy}'
-        if self.energy > 9:
-            return f'{self.energy} '
-        if self.energy <= 9:
-            return f'{self.energy}  '
+    ###        METHODS
+    ### ===========================================================
+    def attack(self, opponent):
+        # Call the combatant attack method, then return damage and attack name for the user interface to use
+        attack_name = self.attack_names[randint(0, len(self.attack_names) - 1)]
+        damage_dealt = super(Dinosaur, self).attack(self, opponent)
+        return (damage_dealt, attack_name)
--- Robot.py
+++ Robot.py
@@ -1,46 +1,18 @@
-## Imports
-#####################################
-import time
-from random import randint
-from Weapon import Weapon
+###        IMPORTS
+### ================================
+from Combatant import Combatant
 
 
-# Robot. Basics are the same as the dinosaur, but has a weapon, power instead of energy, and only one kind of attack
-class Robot:
-    def __init__(self, name):
-        self.name = name
-        self.health = 100
-        self.power = 100
-        weapons = [Weapon("Sword", randint(15, 25)), Weapon("Axe", randint(10, 30)), Weapon("Stuffed Rabbit", randint(1, 50))]
-        self.weapon = weapons[randint(0, len(weapons)-1)]
+# Robot. Implementation of the Combatant class
+class Robot(Combatant):
+    def __init__(self, name, weapon):
+        self.equipped_weapon = weapon
+        super(Robot, self).__init__(name)
 
-    # Deal damage to the selected dinosaur if the robot has enough power
-    def attack(self, dinosaur):
-        if self.power >= 10:
-            dinosaur.health -= self.weapon.attack_power
-            if dinosaur.health < 0:
-                dinosaur.health = 0
-            self.power -= 10
-            print(f"     {self.name.rstrip()} attacks {dinosaur.name} for {self.weapon.attack_power} damage!")
-            time.sleep(1)
-        else:
-            print(f"     {self.name.rstrip()} doesn't have the power to attack!")
-            time.sleep(1)
-
-    # Special formatting for health value
-    def get_health(self):
-        if self.health > 99:
-            return f'{self.health}'
-        if self.health > 9:
-            return f'{self.health} '
-        if self.health <= 9:
-            return f'{self.health}  '
-
-    # Special formatting for power value
-    def get_power(self):
-        if self.power > 99:
-            return f'{self.power}'
-        if self.power > 9:
-            return f'{self.power} '
-        if self.power <= 9:
-            return f'{self.power}  '
+    ###        METHODS
+    ### =================================================================
+    def attack(self, opponent):
+        # Call the combatant attack method, then return damage and attack name for the user interface to use
+        attack_name = self.equipped_weapon.name
+        damage_dealt = super(Robot, self).attack(self, opponent)
+        return (damage_dealt, attack_name)

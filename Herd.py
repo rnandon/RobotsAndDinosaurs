286a6acd53594a26160b22cfdcd8e1fa9546d080
Ryan <ryan@ryannd.com>
2021-07-15 12:25:55 -0500

    Refactored Herd to inherit from Combatant_Group parent class

--- Herd.py
+++ Herd.py
@@ -1,18 +1,31 @@
-## Imports
-######################################
+###        IMPORTS
+### ================================
+from Combatant_Group import Combatant_Group
 from Dinosaur import Dinosaur
 from random import randint
 
 
 
-# Dinosaur container
-class Herd:
-    def __init__(self):
-        self.dinosaurs = []
-        self.create_herd()
+# Dinosaur Combatant Group
+class Herd(Combatant_Group):
+    def __init__(self, names, dinosaur_count=3):
+        # Make sure there are enough names to go around
+        current_dinosaur_count = dinosaur_count
+        if len(names) < current_dinosaur_count:
+            current_dinosaur_count = len(names)
 
-    # Initialize dinosaurs
-    def create_herd(self):
-        names = ["Grr ", "Gar ", "Bill"]
-        for i in range(3):
-            self.dinosaurs.append(Dinosaur(names[i], randint(20, 35)))
+        # Get a list of dino combatants to pass to the Combatant Group constructor
+        dinosaurs = self.create_herd(names, current_dinosaur_count)
+        super(Combatant_Group, self).__init__(dinosaurs)
+
+    ###        METHODS
+    ### =========================================================
+    def create_herd(self, names, dinosaur_count):
+        dinosaurs = []
+        for i in range(dinosaur_count):
+            name = names.pop(randint(0, len(names) - 1))
+            current_attack_power = randint(15, 30)
+            current_robot = Dinosaur(name, current_attack_power)
+            dinosaurs.append(current_robot)
+
+        return dinosaurs

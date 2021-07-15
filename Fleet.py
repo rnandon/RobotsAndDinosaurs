6fff3480a26ff0f6e747d2287dc345dd30e8cfb7
Ryan <ryan@ryannd.com>
2021-07-15 12:20:52 -0500

    Refactored Fleet to inherit from Combatant Group parent class

--- Fleet.py
+++ Fleet.py
@@ -1,16 +1,30 @@
-## Imports
-##################################
+###        IMPORTS
+### ================================
+from Combatant_Group import Combatant_Group
+from random import randint
 from Robot import Robot
 
 
-# Robot container
-class Fleet:
-    def __init__(self):
-        self.robots = []
-        self.create_fleet()
+# Robot Combatant Group
+class Fleet(Combatant_Group):
+    def __init__(self, names, weapons, robot_count=3):
+        # Make sure there are enough names and weapons to go around
+        current_robot_count = robot_count
+        if min(len(weapons), len(names)) < robot_count:
+            current_robot_count = min(len(weapons), len(names))
 
-    # Initialize robots
-    def create_fleet(self):
-        names = ["Bleep     ", "Bloop     ", "Terminator"]
-        for i in range(3):
-            self.robots.append(Robot(names[i]))
+        # Get a list of robot combatants to pass to the Combatant Group constructor
+        robots = self.create_fleet(names, weapons, current_robot_count)
+        super(Combatant_Group, self).__init__(robots)
+
+    ###        METHODS
+    ### =========================================================
+    def create_fleet(self, names, weapons, robot_count):
+        robots = []
+        for i in range(robot_count):
+            weapon = weapons.pop(randint(0, len(weapons) - 1))
+            name = names.pop(randint(0, len(names) - 1))
+            current_robot = Robot(name, weapon)
+            robots.append(current_robot)
+
+        return robots
